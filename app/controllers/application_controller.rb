3df1e89dccff2a0b4fc317536eeb8088702997cf
Stefan Huynh <huynh.stefan@gmail.com>
2014-11-19 00:10:47 -0500

    Add sign in and sign out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  def sign_in!(user)
+    session[:token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def sign_out!
+    current_user.reset_session_token!
+    @current_user = nil
+    session[:token] = nil
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:token])
+  end
+
 end
