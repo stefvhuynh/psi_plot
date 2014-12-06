c06ccdfb78eaa0d3581599d11c5cd6e907106364
Stefan Huynh <huynh.stefan@gmail.com>
2014-12-06 11:15:54 -0500

    Refactor users_controller_spec

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -3,46 +3,50 @@ require 'rails_helper'
 RSpec.describe Api::UsersController, :type => :controller do
 
   describe 'POST #create' do
-    context 'with valid attributes' do
-      before do
-        post :create, user: FactoryGirl.attributes_for(:user), format: :json
-      end
 
-      it 'responds with a 200 OK status' do
+    def make_post_request_with_valid_attrs
+      post :create, user: FactoryGirl.attributes_for(:user), format: :json
+    end
+
+    context 'with valid attributes' do
+      before(run: true) { make_post_request_with_valid_attrs }
+
+      it 'responds with a 200 OK status', run: true do
         expect(response.status).to eq 200
       end
 
       it 'creates and saves a user in the database' do
         expect {
-          post :create, user: FactoryGirl.attributes_for(:user), format: :json
+          make_post_request_with_valid_attrs
         }.to change(User, :count).by 1
       end
 
-      it 'renders user show template' do
+      it 'renders user show template', run: true do
         expect(response).to render_template :show
       end
     end
 
     context 'with invalid attributes' do
-      before do
+
+      def make_post_request_with_invalid_attrs
         post :create,
           user: FactoryGirl.attributes_for(:invalid_user),
           format: :json
       end
 
-      it 'responds with a 422 Unprocessable Entity' do
+      before(run: true) { make_post_request_with_invalid_attrs }
+
+      it 'responds with a 422 Unprocessable Entity', run: true do
         expect(response.status).to eq 422
       end
 
       it 'does not create a user in the database' do
         expect {
-          post :create,
-            user: FactoryGirl.attributes_for(:invalid_user),
-            format: :json
+          make_post_request_with_invalid_attrs
         }.not_to change(User, :count)
       end
 
-      it 'renders with error messages' do
+      it 'renders with error messages', run: true do
         expect(response.body).to include 'errors'
       end
     end
