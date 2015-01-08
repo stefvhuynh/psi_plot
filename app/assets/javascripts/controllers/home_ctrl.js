2d0b438fefab0cc00cf175e1b85c7261e0292d64
Stefan Huynh <huynh.stefan@gmail.com>
2015-01-08 00:47:14 -0500

    Slight refactoring

--- app/assets/javascripts/controllers/home_ctrl.js
+++ app/assets/javascripts/controllers/home_ctrl.js
@@ -1,11 +1,12 @@
-PsiPlotApp.controller('HomeCtrl', ['$scope', 'Auth',
-  function($scope, Auth) {
+PsiPlotApp.controller('HomeCtrl', ['$scope', '$location', 'Auth',
+  function($scope, $location, Auth) {
 
     $scope.auth = { email: '', password: '' };
 
     $scope.submitAuth = function() {
       Auth.save({ session: $scope.auth }, function(response) {
         console.log(response);
+        $location.path('/projects');
       }, function(error) {
         console.log(error);
       });
