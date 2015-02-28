0198f7b25eb202d552c6c0970b887788125785ec
Stefan Huynh <huynh.stefan@gmail.com>
2015-02-27 23:29:25 -0500

    Test chart.js

--- app/assets/javascripts/components/TwoWayPlot.js
+++ app/assets/javascripts/components/TwoWayPlot.js
@@ -1,12 +1,38 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var Chart = require('chart.js');
 
 var TwoWayPlot = React.createClass({
+  componentDidMount() {
+    var context = this.refs['two-way-plot-canvas']
+      .getDOMNode()
+      .getContext('2d');
+
+    new Chart(context).Line({
+      labels: ['first', 'second'],
+      datasets: [
+        {
+          label: 'low',
+          fillColor: 'transparent',
+          strokeColor: 'red',
+          data: [-5, 15]
+        },
+        {
+          label: 'high',
+          fillColor: 'transparent',
+          strokeColor: 'blue',
+          data: [10, 0]
+        }
+      ]
+    });
+  },
+
   render() {
     return(
       <div className="TwoWayPlot">
-        Plot
+        <h4>Line Plot</h4>
+        <canvas ref="two-way-plot-canvas" />
       </div>
     );
   }
